fa432e18a7530a7fba6f8954ee6afd6e361a4593
Reinaldo Landaeta <144262402+REyyyy43@users.noreply.github.com>
2025-08-06 00:09:42 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ const App: React.FC = () => {
         {/* HERO SECTION */}
         <section
           id="inicio"
-          className="flex flex-col md:flex-row items-center min-h-screen px-6 py-16"
+          className="flex flex-col md:flex-row items-center min-h-screen px-6 py-20"
         >
           <div
             className="flex-1 text-center md:text-left"
